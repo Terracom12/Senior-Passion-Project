c1496f9717c60f4088012e550c38473a293af834
Matthew Reese <awesomepop012@hotmail.com>
2023-03-22 08:15:33 -0700

    Completed godot 2d tutorial game

--- Godot-Dodge-Demo/Main.gd
+++ Godot-Dodge-Demo/Main.gd
@@ -3,26 +3,36 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
-
-func _ready():
-	new_game()
-
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	
+	$HUD.show_game_over()
+	
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	
+	get_tree().call_group("mobs", "queue_free")
+	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready...")
+	
+	$Music.play()
 
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
+	$ScoreTimer.start()
 
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_mob_timer_timeout():
 	var mob = mob_scene.instantiate()
@@ -37,3 +47,7 @@ func _on_mob_timer_timeout():
 	
 	add_child(mob)
 	
+
+
+func _on_hud_start_game():
+	new_game()
